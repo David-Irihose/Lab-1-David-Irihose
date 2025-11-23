eba6cb3de8be097d71a856d53198a34bc9dc59f2
David <d.irihose@alustudent.com>
2025-11-23 23:22:41 +0200

    Update organizer.sh

--- organizer.sh
+++ organizer.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # -------------------------------------------
-# Organizer Script - Archives CSV files
+# Organizer Script and Archives CSV files
 # -------------------------------------------
 
-# 1. Check/Create archive directory
+# Check/Create archive directory
 if [ ! -d "archive" ]; then
     mkdir archive
 fi
 
-# 2. Find all .csv files in current directory
+# csv files in current directory
 for file in *.csv; do
     
     # If no CSV files exist, skip
     [ -e "$file" ] || continue
 
-    # 3a. Generate timestamp (YYYYMMDD-HHMMSS)
+    #Generate timestamp (YYYYMMDD-HHMMSS)
     timestamp=$(date +"%Y%m%d-%H%M%S")
 
-    # 3b. Create new filename
+    #Create new filename
     base="${file%.csv}"
     newname="${base}-${timestamp}.csv"
 
-    # 3c. Log the action + file contents
+    # Log the action + file contents
     echo "----------------------------------------" >> organizer.log
     echo "Archived: $file  --> archive/$newname" >> organizer.log
     echo "Timestamp: $timestamp" >> organizer.log
@@ -30,7 +30,7 @@ for file in *.csv; do
     cat "$file" >> organizer.log
     echo "" >> organizer.log
 
-    # 3d. Move & rename the CSV
+    #Move & rename the CSV
     mv "$file" "archive/$newname"
 
 done
